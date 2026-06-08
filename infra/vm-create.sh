f84c8558a948e094d03d0821169ba5030398d7f6
Stephen Mesa <stephen.mesa@gmail.com>
2026-06-08 09:11:48 -0700

    misc build and infra scripts

---
+++ infra/vm-create.sh
@@ -0,0 +1,17 @@
+gcloud compute instances create ef2-discord-bot \
+    --project=ef2-discord-bot \
+    --zone=us-central1-c \
+    --machine-type=e2-micro \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+    --maintenance-policy=MIGRATE \
+    --provisioning-model=STANDARD \
+    --service-account=ef2-discord-bot@ef2-discord-bot.iam.gserviceaccount.com \
+    --scopes=https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
+    --create-disk=auto-delete=yes,boot=yes,device-name=ef2-discord-bot,image=projects/cos-cloud/global/images/cos-117-18613-613-40,mode=rw,size=10,type=pd-balanced \
+    --no-shielded-secure-boot \
+    --shielded-vtpm \
+    --shielded-integrity-monitoring \
+    --labels=goog-ec-src=vm_add-gcloud \
+    --reservation-affinity=any \
+    --metadata-from-file=user-data=cloud-config.yaml \
+    --metadata=google-logging-enabled=true
